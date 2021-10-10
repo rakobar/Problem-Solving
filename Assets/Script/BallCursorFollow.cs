398d5fdac3b6d8cbfb49c9b4342919662e47671d
rezelion <ahmadspade2014@gmail.com>
2021-10-10 20:03:11 +0700

    Full Problem 5

---
+++ Assets/Script/BallCursorFollow.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallCursorFollow : MonoBehaviour
+{
+    // RigidBody Bola
+    private Rigidbody2D RB;
+
+    // Inisialisasi Kecepatan & posisi Mouse
+    private Vector3 mousePos;
+    public float speed;
+
+    private void Start()
+    {
+        RB = GetComponent<Rigidbody2D>();
+    }
+    private void FixedUpdate()
+    {
+        mousePos = Input.mousePosition; // get posisi cursor
+        mousePos = Camera.main.ScreenToWorldPoint(mousePos); // get area screen 
+        transform.position = Vector2.MoveTowards(transform.position, mousePos, speed * Time.deltaTime); // agar bergerak mengikuti mouse
+    }
+}
